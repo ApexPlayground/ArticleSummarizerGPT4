bcbcf0c66b32699b31a925086f952c63b83d5b0a
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-04-21 23:34:12 +0100

    Changed the style a bit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
+import App from "./App.jsx";
+import { store } from "./services/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      {" "}
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
